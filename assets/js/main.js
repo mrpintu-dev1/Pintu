ad3c8c5108d21e6bb18b172c7c8fe7e9559c3f66
mrpintu-dev1 <mrpintudev1@gmail.com>
2021-11-07 11:03:35 +0600

    Adding About portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,28 @@
         }
     })
 
+    $(".skillbar").skillbar({
+        speed: 1000
+    });
+
+    $('.image-link').magnificPopup({type:'image'});
+
+
+    // custom cursor
+    const cursor = document.querySelector('.cursor');
+
+    document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style","top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px")
+    })
+
+    document.addEventListener('click', () => {
+        cursor.classList.add("expand");
+
+        setTimeout(() => {
+            cursor.classList.remove("expand");
+        }, 500)
+    })
+
 
     /*------ Wow Active ----*/
     new WOW().init();
